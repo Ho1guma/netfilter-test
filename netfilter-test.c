027ef33dc051babc91544d12b588c84179c79f3e
sumin <sumin816@ewhain.net>
2021-08-16 09:39:56 -0400

    netfilter

---
+++ netfilter-test.c
@@ -0,0 +1,251 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <linux/types.h>
+#include <linux/netfilter.h>		/* for NF_ACCEPT */
+#include <errno.h>
+#include <libnetfilter_queue/libnetfilter_queue.h>
+#include <libnet.h>
+#include <string.h>
+
+struct libnet_ipv4_hdr* check_ip_header;
+struct libnet_ipv4_hdr* ip_header;
+struct libnet_tcp_hdr* check_tcp_header;
+struct libnet_tcp_hdr* tcp_header;
+
+char* input;
+int flag=0;
+
+char *strnstr(const char *s1, const char *s2, size_t len)
+{
+    size_t l2;
+    l2 = strlen(s2);
+    if (!l2)
+        return (char *)s1;
+    while (len >= l2) {
+        len--;
+        if (!memcmp(s1, s2, l2))
+            return (char *)s1;
+        s1++;
+    }
+    return NULL;
+}
+
+
+int FindHost(char* buf, char* hostname)
+{
+    char *HostTag="Host";
+    char *hostfield = strtok((char*)buf, "\r\n"); //split buf using \r\n
+
+
+    while (hostfield != NULL)
+        {
+            if(strnstr(hostfield,HostTag,strlen(hostfield))!=NULL) //Find host field using 'Host'
+            {
+                if(strnstr(hostfield,hostname,strlen(hostfield))!=NULL) //match the hostname and host field
+                {
+                    return 0; //match!
+                }
+                return -1; //find host field but not matched..
+             }
+            hostfield = strtok(NULL, "\r\n");
+        }
+    return -1;
+
+}
+
+void dump(unsigned char* buf, int size) {
+    int i;
+    for (i = 0; i < size; i++) {
+        if (i != 0 && i % 16 == 0)
+            printf("\n");
+        printf("%02X ", buf[i]);
+    }
+    printf("\n");
+}
+
+int check_site(unsigned char* buf, int size){
+        u_int8_t ip_header_size = 20;
+
+        check_ip_header = (struct libnet_ipv4_hdr *)(buf);
+        if(check_ip_header->ip_p != IPPROTO_TCP)
+            return -1;
+
+        buf = buf + ip_header_size;
+
+        check_tcp_header = (struct libnet_tcp_hdr *)(buf);
+        u_int8_t tcp_header_size = check_tcp_header->th_off*4;
+
+        buf = buf + tcp_header_size;
+
+        int payload_size;
+        payload_size= size - ip_header_size - tcp_header_size;
+
+
+        if(FindHost((char*)buf,input)==0)
+        {
+            printf("detect!!!\n");
+            flag=1;
+        }
+
+        return 0;
+}
+
+/* returns packet id */
+static u_int32_t print_pkt (struct nfq_data *tb)
+{
+    int id = 0;
+    struct nfqnl_msg_packet_hdr *ph;
+    struct nfqnl_msg_packet_hw *hwph;
+    u_int32_t mark,ifi;
+    int ret;
+    unsigned char *data;
+
+    ph = nfq_get_msg_packet_hdr(tb);
+    if (ph) {
+        id = ntohl(ph->packet_id);
+        printf("hw_protocol=0x%04x hook=%u id=%u ",
+            ntohs(ph->hw_protocol), ph->hook, id);
+    }
+
+    hwph = nfq_get_packet_hw(tb);
+    if (hwph) {
+        int i, hlen = ntohs(hwph->hw_addrlen);
+
+        printf("hw_src_addr=");
+        for (i = 0; i < hlen-1; i++)
+            printf("%02x:", hwph->hw_addr[i]);
+        printf("%02x ", hwph->hw_addr[hlen-1]);
+    }
+
+    mark = nfq_get_nfmark(tb);
+    if (mark)
+        printf("mark=%u ", mark);
+
+    ifi = nfq_get_indev(tb);
+    if (ifi)
+        printf("indev=%u ", ifi);
+
+    ifi = nfq_get_outdev(tb);
+    if (ifi)
+        printf("outdev=%u ", ifi);
+    ifi = nfq_get_physindev(tb);
+    if (ifi)
+        printf("physindev=%u ", ifi);
+
+    ifi = nfq_get_physoutdev(tb);
+    if (ifi)
+        printf("physoutdev=%u ", ifi);
+
+    ret = nfq_get_payload(tb, &data);
+    if (ret >= 0)
+    {
+        printf("payload_len=%d\n", ret);
+        //dump(data,ret);
+        check_site(data,ret);
+    }
+    fputc('\n', stdout);
+
+    return id;
+}
+
+
+static int cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg,
+          struct nfq_data *nfa, void *data)
+{
+    u_int32_t id = print_pkt(nfa);
+    printf("entering callback\n");
+    if(flag==1)
+    {
+        printf("Block Hazardous Sites\n");
+        flag=0;
+        return nfq_set_verdict(qh, id, NF_DROP, 0, NULL);
+    }
+    return nfq_set_verdict(qh, id, NF_ACCEPT, 0, NULL);
+}
+
+int main(int argc, char **argv)
+{
+    struct nfq_handle *h;
+    struct nfq_q_handle *qh;
+    struct nfnl_handle *nh;
+    int fd;
+    int rv;
+    char buf[4096] __attribute__ ((aligned));
+    printf("%s",argv[1]);
+    input = malloc(strlen(argv[1]) + 1);
+    strcpy(input,argv[1]);
+
+    printf("opening library handle\n");
+    h = nfq_open();
+    if (!h) {
+        fprintf(stderr, "error during nfq_open()\n");
+        exit(1);
+    }
+
+    printf("unbinding existing nf_queue handler for AF_INET (if any)\n");
+    if (nfq_unbind_pf(h, AF_INET) < 0) {
+        fprintf(stderr, "error during nfq_unbind_pf()\n");
+        exit(1);
+    }
+
+    printf("binding nfnetlink_queue as nf_queue handler for AF_INET\n");
+    if (nfq_bind_pf(h, AF_INET) < 0) {
+        fprintf(stderr, "error during nfq_bind_pf()\n");
+        exit(1);
+    }
+
+    printf("binding this socket to queue '0'\n");
+
+    qh = nfq_create_queue(h,  0, &cb, NULL);
+    if (!qh) {
+        fprintf(stderr, "error during nfq_create_queue()\n");
+        exit(1);
+    }
+
+    printf("setting copy_packet mode\n");
+    if (nfq_set_mode(qh, NFQNL_COPY_PACKET, 0xffff) < 0) {
+        fprintf(stderr, "can't set packet_copy mode\n");
+        exit(1);
+    }
+
+    fd = nfq_fd(h);
+
+    for (;;) {
+        if ((rv = recv(fd, buf, sizeof(buf), 0)) >= 0) {
+            printf("pkt received\n");
+            nfq_handle_packet(h, buf, rv);
+            continue;
+        }
+        /* if your application is too slow to digest the packets that
+         * are sent from kernel-space, the socket buffer that we use
+         * to enqueue packets may fill up returning ENOBUFS. Depending
+         * on your application, this error may be ignored. nfq_nlmsg_verdict_putPlease, see
+         * the doxygen documentation of this library on how to improve
+         * this situation.
+         */
+        if (rv < 0 && errno == ENOBUFS) {
+            printf("losing packets!\n");
+            continue;
+        }
+        perror("recv failed");
+        break;
+    }
+
+    printf("unbinding from queue 0\n");
+    nfq_destroy_queue(qh);
+
+#ifdef INSANE
+    /* normally, applications SHOULD NOT issue this command, since
+     * it detaches other programs/sockets from AF_INET, too ! */
+    printf("unbinding from AF_INET\n");
+    nfq_unbind_pf(h, AF_INET);
+#endif
+
+    printf("closing library handle\n");
+    nfq_close(h);
+
+    exit(0);
+}
+
